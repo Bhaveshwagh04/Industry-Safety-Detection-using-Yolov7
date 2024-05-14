54df29ddb05950aea9692465a78a7775e01d77cc
Bhaveshwagh04 <121219011+Bhaveshwagh04@users.noreply.github.com>
2024-05-14 15:35:59 +0530

    setup added

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-
 from setuptools import find_packages, setup
 
 setup(
-    
-    name = "isd",
-    version = "0.0.0",
-    author = "Bhavesh Wagh",
-    author_email= "bhaveshwaghbw.49@gmail.com",
-    packages= find_packages.py()
-    install_requires = []
+    name="isd",
+    version="0.0.0",
+    author="Bhavesh wagh",
+    author_email="bhaveshwaghbw.49@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
 )
